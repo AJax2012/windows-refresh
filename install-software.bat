b436dd090af4339df5e837c20ca9c6e88f1d6838
AJ Gardner <agardner003@regis.edu>
2018-11-14 13:11:13 -0700

    added params and added forced yes to select files

--- install-software.bat
+++ install-software.bat
@@ -3,20 +3,19 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 :: Install all the packages
-:: -y confirm yes for any prompt during the install process
 
 :: Git
-choco install git -fy
-choco install tortoisegit -fy
+choco install git
+choco install tortoisegit
 
 :: Browsers
-choco install googlechrome -fy
+choco install googlechrome -fy -params "/AddToDesktop"
 choco install firefox -fy
 choco install opera -fy
 
 :: Communication tools
 choco install slack -fy
-choco install discord -fy
+choco install discord -fy -params "/AddToDesktop"
 
 :: Testing tools
 choco install postman -fy
@@ -25,52 +24,49 @@ choco install postman -fy
 choco install nodejs -fy
 
 :: Server tools
-choco install sql-server-express -fy
-choco install sql-server-management-studio -fy
-choco install sqlite -fy
-choco install mysql -fy
-choco install mysql.workbench -fy
+choco install sql-server-express 
+choco install sql-server-management-studio -params "/PinnedToTaskbar"
+choco install sqlite 
+choco install mysql 
+choco install mysql.workbench 
+choco install mysql-connector
 
 :: Java
 choco install jdk8 -fy
-choco install jre8 -fy
-
-:: Android
-choco install android-sdk -fy
+choco install jre8 -gy
 
 :: IDEs and text editors
-choco install vscode -fy
+choco install vscode -fy -params '"/PinnedToTaskbar /AddToDesktop"'
 choco install visualstudio2017-installer -fy
 hoco install resharper-ultimate-all -fy
-choco install webstorm -fy
+choco install webstorm -fy -params '"/PinnedToTaskbar /AddToDesktop"'
 choco install intellijidea-ultimate -fy
 choco install sublimetext3 -fy
-choco install androidstudio -fy
+choco install androidstudio
 choco install brackets -fy
 choco install springtoolsuite -fy
 choco install arduino -fy
-choco install notepadplusplus.install -fy
 choco install jetbrainstoolbox -fy
 
 :: VM tools
-choco install virtualbox -fy
+choco install virtualbox -params " /AddToDesktop"
 
 :: Win Admin tools
-choco install winscp -fy
-choco install putty -fy
-choco install everything -fy
+choco install winscp -fy -params "/AddToDesktop"
+choco install putty -fy -params "/AddToDesktop"
+choco install everything -params "/AddToDesktop"
 choco install cpu-z -fy
 choco install 7zip -fy
 choco install msiafterburner -fy
 
 :: Entertainment
-choco install gimp -fy
+choco install gimp -fy 
 choco install vlc -fy
-choco install steam -fy
-choco install origin -fy
+choco install steam -fy -params "/AddToDesktop"
+choco install origin -fy -params "/AddToDesktop"
 
 :: NPM installs
-npm install -g @angular/cli -fy
-npm install -g cordova ionic -fy
+npm install -g @angular/cli 
+npm install -g cordova ionic 
 
 :: cannot install office or adobe cc
