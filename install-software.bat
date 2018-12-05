446d1a62420a03e1d0835e6067a89614c4616e6f
unknown <ajax@DESKTOP-58A10IL.ajax2012.local>
2018-12-05 15:31:42 -0700

    -fy to all

--- install-software.bat
+++ install-software.bat
@@ -5,17 +5,10 @@
 :: Install all the packages
 
 :: Git
-choco install git
-choco install tortoisegit
-
-:: Browsers
-choco install googlechrome -fy -params "/AddToDesktop"
-choco install firefox -fy
-choco install opera -fy
+choco install tortoisegit -fy
 
 :: Communication tools
 choco install slack -fy
-choco install discord -fy -params "/AddToDesktop"
 
 :: Testing tools
 choco install postman -fy
@@ -24,23 +17,21 @@ choco install postman -fy
 choco install nodejs -fy
 
 :: Server tools
-choco install sql-server-express 
-choco install sql-server-management-studio -params "/PinnedToTaskbar"
-choco install sqlite 
-choco install mysql 
-choco install mysql.workbench 
-choco install mysql-connector
+choco install sql-server-express -fy
+choco install sql-server-management-studio -fy -params "/PinnedToTaskbar"
+choco install sqlite -fy
+choco install mysql -fy
+choco install mysql.workbench -fy
+choco install mysql-connector -fy
 
 :: Java
 choco install jdk8 -fy
-choco install jre8 -gy
+choco install jre8 -fy
 
 :: IDEs and text editors
 choco install vscode -fy -params '"/PinnedToTaskbar /AddToDesktop"'
 choco install visualstudio2017-installer -fy
 hoco install resharper-ultimate-all -fy
-choco install webstorm -fy -params '"/PinnedToTaskbar /AddToDesktop"'
-choco install intellijidea-ultimate -fy
 choco install sublimetext3 -fy
 choco install androidstudio
 choco install brackets -fy
@@ -54,19 +45,13 @@ choco install virtualbox -params " /AddToDesktop"
 :: Win Admin tools
 choco install winscp -fy -params "/AddToDesktop"
 choco install putty -fy -params "/AddToDesktop"
-choco install everything -params "/AddToDesktop"
 choco install cpu-z -fy
-choco install 7zip -fy
 choco install msiafterburner -fy
 
 :: Entertainment
-choco install gimp -fy 
 choco install vlc -fy
-choco install steam -fy -params "/AddToDesktop"
 choco install origin -fy -params "/AddToDesktop"
 
 :: NPM installs
 npm install -g @angular/cli 
-npm install -g cordova ionic 
-
-:: cannot install office or adobe cc
+npm install -g cordova ionic
